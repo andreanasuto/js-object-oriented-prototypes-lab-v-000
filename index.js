5b66ccb9247b42d1aabf3ee9ae0d4cb7d764fdc0
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-03-03 20:09:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,21 +6,21 @@
 //   this.veto = function(){
 //     return 'No, I must disagree'
 //   }
-   this.approve = function(){
-     return 'You can do that!'
-   }
-   this.doCharity = function(){
-     return "I like to help people."
-   }
+//   this.approve = function(){
+//     return 'You can do that!'
+//   }
+//   this.doCharity = function(){
+//     return "I like to help people."
+//   }
+//
+//  this.releasePressStatement = function(){
+//     return "You will see great things from Scuber."
+//  }
 
-   this.releasePressStatement = function(){
-     return "You will see great things from Scuber."
-   }
-
-   this.sayHi = function(){
-     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-   }
-  }
+//   this.sayHi = function(){
+//     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+//   }
+// }
 
 BoardMember.prototype.veto = function () {
   return 'No, I must disagree'
